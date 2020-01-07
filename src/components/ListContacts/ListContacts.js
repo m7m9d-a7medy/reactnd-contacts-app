e1e3cb4df9b689ebe798b9cbe6ee89d7abff7c84
Mahmoud Ahmedy <m7m9d.a7medy@gmail.com>
2020-01-08 00:18:52 +0200

    functional-list-component

--- src/components/ListContacts/ListContacts.js
+++ src/components/ListContacts/ListContacts.js
@@ -1,38 +1,34 @@
-import React, {Component} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-class ListContacts extends Component {
-  constructor(props){
-    super(props);
-  }
-  render(){
-    return (
-      <ol className="contact-list">
-        { this.props.contacts.map(contact => {
-        return (
-          <li key={contact.id} className="contact-list-item">
-            <div
-              className="contact-avatar"
-              style={{
-                backgroundImage: `url(${contact.avatarURL})`
-              }}
-            ></div>
+const ListContacts = (props) => {
+  return (
+    <ol className="contact-list">
+      { props.contacts.map(contact => {
+      return (
+        <li key={contact.id} className="contact-list-item">
+          <div
+            className="contact-avatar"
+            style={{
+              backgroundImage: `url(${contact.avatarURL})`
+            }}
+          ></div>
 
-            <div className='contact-details'>
-                <p>{contact.name}</p>
-                <p>{contact.handle}</p>
-            </div>
+          <div className='contact-details'>
+              <p>{contact.name}</p>
+              <p>{contact.handle}</p>
+          </div>
 
-            <button className='contact-remove'>
-              Remove
-            </button>
-          </li>)
-        })}
+          <button className='contact-remove'>
+            Remove
+          </button>
+        </li>)
+      })}
 
-      </ol>
-    )
-  }
-}
+    </ol>
+  )
+};
+
 ListContacts.propTypes = {
   contacts: PropTypes.arrayOf(PropTypes.shape({
     id: PropTypes.string.isRequired,
